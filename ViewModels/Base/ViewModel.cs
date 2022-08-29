86e9d9c577484b4541eabb24d63ca4466da7701f
Danil Shupletsov <shupletsovdanil31@gmail.com>
2022-08-29 17:39:41 +0700

    Шаблон BaseViewModels

--- ViewModels/Base/ViewModel.cs
+++ ViewModels/Base/ViewModel.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace WpfElmaBot_2._0_.ViewModels.Base
 {
-    internal abstract class ViewModel
+    internal abstract class ViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public virtual void OnPropertyChanged([CallerMemberName] string PropertyName=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if(Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+
     }
 }
